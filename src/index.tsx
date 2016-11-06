b40821e9980933c12a1fef8528e6c4563878e609
lewl <Lew.Ladd@alseageo.net>
2016-11-06 11:30:30 -0800

    Intermediate work.

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,22 @@ import {RocketLauncherView} from "./RocketLauncherView";
 import {Actions} from "./Actions";
 import {Model} from "./Model";
 import {State} from "./State";
+
+
 import {Bmi} from "./bmi/Bmi";
+import {ActionsBmi} from "./bmi/ActionsBmi";
+import {ModelBmi} from "./bmi/ModelBmi";
+import {StateBmi} from "./bmi/StateBmi";
 
 // Instantiating the SAM pattern
 const {state} = SAMFactory.instance(Actions, Model, State);
+const stateLauncher = state;
+
+const {state} = SAMFactory.instance(ActionsBmi, ModelBmi, StateBmi);
+const stateBmi = state;
 
 ReactDOM.render(
     <div>
-    <RocketLauncherView stateRepresentation={state}/>
-        <Bmi stateRepresentation={state}></Bmi>
+    <RocketLauncherView stateRepresentation={stateLauncher}/>
+        <Bmi stateRepresentation={stateBmi}></Bmi>
     </div>, document.getElementById('root'));
